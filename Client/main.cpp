2cbeb460417212f30655909b2ca91b20fa87fbdd
Keith <hnefatl@gmail.com>
2015-03-17 17:31:04 +0000

    Prepared for next stage of development

--- Client/main.cpp
+++ Client/main.cpp
@@ -8,34 +8,15 @@ int main(int argc, char *argv[])
 	if (WSANeeded())
 		if (!WSAStart())
 			return -1;
-	
-	while (true)
-	{
-		std::string Address;
-		std::cout << "Enter address: ";
-		std::getline(std::cin, Address);
 
-		SOCKET Client;
-		if (!Create(Client) || !Connect(Client, Address, true, 34652))
-		{
-			std::cout << "Failed to connect." << std::endl;
-			continue;
-		}
 
-		while (true)
-		{
-			std::string Message;
-			std::cout << "Enter message: ";
-			std::getline(std::cin, Message);
 
-			if (!Send(Client, Message))
-			{
-				std::cout << "Server disconnected" << std::endl;
-				break;
-			}
-		}
-	}
 
+
+
+	if (WSANeeded())
+		if (!WSAClose())
+			return -1;
 
 	return 0;
 }
--- Server/main.cpp
+++ Server/main.cpp
@@ -9,39 +9,14 @@ int main(int argc, char *argv[])
 		if (!WSAStart())
 			return -1;
 
-	SOCKET ServerSocket;
-	if (!Create(ServerSocket))
-		return -2;
 
-	if (!Bind(ServerSocket, 34652))
-		return -3;
 
-	if (!Listen(ServerSocket, 1))
-		return -4;
 
-	while (true)
-	{
-		SOCKET Client;
-		std::string Address;
-		bool IsIPV4;
-		unsigned short Port;
-		if (!Accept(ServerSocket, Client, &Address, &IsIPV4, &Port))
-			break;
 
-		while (true)
-		{
-			std::string Message;
-			if (!ReceiveString(Client, &Message))
-			{
-				std::cout << "Client disconnected" << std::endl;
-				break;
-			}
 
-			std::cout << Message << std::endl;
-		}
-	}
-
-	WSAClose();
+	if (WSANeeded())
+		if (!WSAClose())
+			return -1;
 
 	return 0;
 }
